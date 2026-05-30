5ab09498de51b6f6b8d1cf5db031555af1a45865
exsvysq <xzvhel8@gmail.com>
2026-05-30 14:40:30 -0400

    Add files via upload

---
+++ Backend.php
@@ -0,0 +1,30 @@
+<?php
+// Backend.php
+// This file is used to manage various text and URL data on the webpage.
+
+$backend_data = [
+
+    // --- Page Meta Information ---
+    'page_title'        => 'WoundSMP', // Title for the browser tab
+	
+    // --- Main Text Section ---
+    'main_title'        => 'WoundSMP',
+    'subtitle_text'     => '',
+    'description_text'  => 'Join a new Economy SMP today!',
+
+    // --- Button Text Section ---
+    'store_button_text'   => 'Store (In Progress)',
+    'store_button_url'    => 'https:/woundsmp.site', // Add your store URL here
+    'discord_button_text' => 'Discord',
+    'discord_button_url'  => 'https://discord.gg/Rqp33c54Yc', // Add your Discord invite URL here
+    'play_button_text'    => 'Play Now',
+
+    // --- Server Information Section ---
+    'server_ip'         => 'play.WoundSMP.site', // Server IP Address
+    'minecraft_version' => '1.8x - 1.21+',   // Supported Minecraft versions
+    'api_url'           => 'https://api.mcsrvstat.us/3/play.woundsmp.site' // Delete Hypixel.net ip and change it to your server ip
+
+//  													 ***EXAMPLE***
+//  'api_url'           => 'https://api.mcsrvstat.us/3/<Your server IP>' // Delete Hypixel.net ip and change it to your server ip
+];
+?>
